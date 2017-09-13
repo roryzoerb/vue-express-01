3088a4770e8cf096a1adc5c5a048a5aedcade6c0
roryzoerb <rory.zoerb@gmail.com>
2017-09-13 04:05:13 -0600

    Setup of Express.js server

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyparser.json())
+// security concern (cors)
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world!'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
